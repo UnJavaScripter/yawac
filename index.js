36d1178e71266bdd8a9aa2a4001d597e153f2713
UnJavaScripter <eltu.khan@gmail.com>
2016-04-27 19:03:17 -0500

    fetch recursively

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const cheerio = require('cheerio');
 
 console.log(JSON.stringify(process.env.URL))
 const baseUrl = process.env.URL;
-let url = baseUrl + '/';
+let route = '/'
 
-const handleError = (err) => {console.log(err)}
+let routesToFetch = [route];
+
+
+const handleError = (err) => {console.error(err)}
 
 
 const writeJsonFile = (records, fileName) =>{
@@ -20,48 +23,88 @@ const writeJsonFile = (records, fileName) =>{
     });
 }
 
+let report = [];
 
+const fetchRoute = (route) => {
+    let url = baseUrl + route;
 
+    request(url, (err, response, html) => {
+        if(!err){
+            let $ = cheerio.load(html);
+            let linkTag = $('link');
+            let scriptTag = $('script');
+            let imgTag = $('img');
+            let anchorTag = $('a');
 
-request(url, (err, response, html) => {
-    if(!err){
-        let $ = cheerio.load(html);
-        let linkTag = $('link');
-        let scriptTag = $('script');
-        let imgTag = $('img');
+            let assets = {
+                icon:[],
+                stylesheet: [],
+                script: [],
+                img: []
+            };
 
-        let assets = {
-            icon:[],
-            stylesheet: [],
-            script: [],
-            img: []
-        };
+            let anchors = [];
 
-        // Stylesheets & icons
-        $(linkTag).each((i, link) => {
-            if($(link).attr('rel') === 'stylesheet' || $(link).attr('rel') === 'icon'){
-                assets[$(link).attr('rel')].push($(link).attr('href'));
-            }
-        });
+            // Stylesheets & icons
+            $(linkTag).each((i, link) => {
+                if($(link).attr('rel') === 'stylesheet' || $(link).attr('rel') === 'icon'){
+                    assets[$(link).attr('rel')].push($(link).attr('href'));
+                }
+            });
 
-        // Scripts
-        $(scriptTag).each((i, script) =>{
-            if($(script).attr('src')){
-                assets.script.push($(script).attr('src'));
-            }
-        })
+            // Scripts
+            $(scriptTag).each((i, script) =>{
+                if($(script).attr('src')){
+                    assets.script.push($(script).attr('src'));
+                }
+            });
 
-        // Images
-        $(imgTag).each((i, img) =>{
-            if($(img).attr('src')){
+            // Images
+            $(imgTag).each((i, img) =>{
                 assets.img.push($(img).attr('src'));
-            }
-        })
+            });
 
-        console.log(assets)
-    }else{
-        handleError(err);
-    }
+            // Links
+            $(anchorTag).each((i, a) =>{
+                if(/^\//g.test($(a).attr('href')) && routesToFetch.indexOf($(a).attr('href')) === -1 ){
+                    anchors.push({
+                        text: $(a).text(),
+                        link: $(a).attr('href'),
+                        parent: route
+                    });
+
+                    
+                    routesToFetch.push($(a).attr('href'));
+                    
+                }
+            });
+
+            report.push({
+                [route]: {
+                    route: route,
+                    assets: assets,
+                    links: anchors
+                }
+            });
 
 
-});
+
+            routesToFetch.forEach((aRoute, i, a)=> {
+                console.log("will fetch", aRoute)
+                fetchRoute(aRoute);
+            });
+
+            console.log('*************************************************************', routesToFetch.length);
+
+        }else{
+            handleError(err);
+        }
+    })
+}
+
+fetchRoute(route);
+
+
+console.log(routesToFetch)
+console.log('------------------------------------------------------------------------------------------')
+console.log(report)
