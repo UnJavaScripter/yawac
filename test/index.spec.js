6182740985b252d0a07c39e328b99fd616cceff3
UnJavaScripter <eltu.khan@gmail.com>
2016-05-01 11:01:19 -0500

    add promise to main function
    make tests work with promises instead of ugly timeouts

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,34 @@
 "use strict";
 
-let assert = require('chai').assert;
+let chai = require("chai");
+let chaiAsPromised = require("chai-as-promised");
+let assert = chai.assert;
 let yawac = require('../lib/yawac')
 let report;
 
 describe('yawac tests: ', function()  {
-  this.timeout(10000);
   before(() => {
     yawac.baseUrl = 'http://unjavascripter.github.io/arraify';
     report = yawac.fetchRoute();
   });
 
-  // Ugly hack, I know :/
-  it('should give the test a little bit of time', (done) => {
-    setTimeout(() => done(), 2000);
-  });
-
   it('should have a single record', () => {
-      assert.equal(1, report.length);
+    report.then(() => {
+        assert.equal(1, report.length);
+    });
   });
 
   it('should have a single item on stylesheet & script array ', () => {
-    assert.equal(1, report[0]["/"].assets.stylesheet.length);
-    assert.equal(1, report[0]["/"].assets.script.length);
+    report.then(() => {
+      assert.equal(1, report[0]["/"].assets.stylesheet.length);
+      assert.equal(1, report[0]["/"].assets.script.length);
+    });
   });
 
   it('should have no links', () => {
+    report.then(() => {
       assert.equal(0, report[0]["/"].links.length);
+    });
   });
 
 });
