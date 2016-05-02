fe3402b8635d4a53daca3e251c46cc592663ca88
UnJavaScripter <eltu.khan@gmail.com>
2016-05-02 00:38:54 -0500

    add readme
    update tests

---
+++ README.md
@@ -0,0 +1,65 @@
+# YAWAS
+
+## Wut???
+
+Stands for Yet Another Web App Scrapper. It's a simple CLI tool that fetches local routes of a given web(site|app) and creates a JSON file containing a list of the routes with the assets (icons, CSS & JavaScript file references and images) and links inside that page.
+
+## Requirements
+
+- [Nodejs 4 or greater](https://github.com/nodesource/distributions)
+
+## Installation
+
+Business as usual `npm install`
+
+## Playing with it
+
+It expects a URL parameter which is the base where it should start scrapping (it can be a subdomain). YAWAS will follow every link inside that first page and the subsequent pages until ther are no more left.
+  
+  Note that it will only follow local routes like `/cool/things/here` and will skip external links like `http://commitstrip.com`.
+
+Sample: `URL='http://unjavascripter.github.io/testrepo' node index.js`
+
+### The report
+
+A `siteData.json` file will be created in the root folder, it will have the following contents for the _testrepo_ sample:
+
+```json
+[
+    {
+        "/": {
+            "assets": {
+                "icon": [],
+                "stylesheet": [],
+                "script": [
+                    "/theScript.js"
+                ],
+                "img": [
+                    "xomg.jpg"
+                ]
+            },
+            "links": [
+                {
+                    "text": "Click here",
+                    "link": "/noonecanfindme.html",
+                    "parent": "/"
+                }
+            ]
+        }
+    },
+    {
+        "/noonecanfindme.html": {
+            "assets": {
+                "icon": [],
+                "stylesheet": [
+                    "/styyyyles.css"
+                ],
+                "script": [],
+                "img": []
+            },
+            "links": []
+        }
+    }
+]
+
+```
--- test/index.spec.js
+++ test/index.spec.js
@@ -8,27 +8,89 @@ let report;
 
 describe('yawac tests: ', function()  {
   before(() => {
-    yawac.baseUrl = 'http://unjavascripter.github.io/arraify';
+    yawac.baseUrl = 'http://unjavascripter.github.io/testrepo';
     report = yawac.fetchRoute();
   });
 
-  it('should have a single record', () => {
-    report.then(() => {
-        assert.equal(1, report.length);
+  
+
+  describe('Page 1 (index)', () => {
+
+    it('should have no items on the icon assets array', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[0]["/"].assets.icon.length);
+        done();
+      });
+    });
+
+    it('should have no stylesheet references', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[0]["/"].assets.stylesheet.length);
+        done();
+      });
+    });
+
+
+    it('should have one script reference', (done) => {
+      report.then((r) => {
+        assert.equal(1, r[0]["/"].assets.script.length);
+        done();
+      });
+    });
+
+    it('should have one image reference', (done) => {
+      report.then((r) => {
+        assert.equal(1, r[0]["/"].assets.img.length);
+        done();
+      });
+    });
+
+    it('should have one link', (done) => {
+      report.then((r) => {
+        assert.equal(1, r[0]["/"].links.length);
+        done();
+      });
+    });
+  })
+
+  describe('Page 2 (noonecanfindme)', () => {
+
+    it('should have no items on the icon assets array', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[1]["/noonecanfindme.html"].assets.icon.length);
+        done();
+      });
+    });
+
+    it('should have one stylesheet reference', (done) => {
+      report.then((r) => {
+        assert.equal(1, r[1]["/noonecanfindme.html"].assets.stylesheet.length);
+        done();
+      });
+    });
+
+
+    it('should have no script reference', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[1]["/noonecanfindme.html"].assets.script.length);
+        done();
+      });
+    });
+
+    it('shouldn\'t have image references', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[1]["/noonecanfindme.html"].assets.img.length);
+        done();
+      });
+    });
+
+    it('should have no links', (done) => {
+      report.then((r) => {
+        assert.equal(0, r[1]["/noonecanfindme.html"].links.length);
+        done();
+      });
     });
   });
 
-  it('should have a single item on stylesheet & script array ', () => {
-    report.then(() => {
-      assert.equal(1, report[0]["/"].assets.stylesheet.length);
-      assert.equal(1, report[0]["/"].assets.script.length);
-    });
-  });
-
-  it('should have no links', () => {
-    report.then(() => {
-      assert.equal(0, report[0]["/"].links.length);
-    });
-  });
 
 });
